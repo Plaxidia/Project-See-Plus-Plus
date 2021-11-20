21f7024dc42dfd22eadb656ef418e9a22b503f8e
plaxidia <plaxidiahove@gmail.com>
2021-11-20 19:49:17 +0300

    first commit

---
+++ task1.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+//
+//A = 3 * i + 5 * j + 4 * k
+//B = 2 * i + 7 * j + 5 * k
+//dot product = 3 * 2 + 5 * 7 + 4 * 5
+//            = 6 + 35 + 20
+//            = 61
+
+//The first task is to implement a function, that computes dot product of two vectors of a given size.}
+ 
+/*double  rand(double const n,double vector_a[], double  vector_b[],double random){
+   
+ double random;
+       srand((unsigned) time(0));
+        
+        if(n<=5){
+            cout<<"enter the values of vector_a:"<<endl;
+               for (int i = 0 ; i<n; i++) {
+                   cin>>vector_a[i];
+               }
+               
+               cout<<"enter the values of vector_b"<<endl;
+               for (int i = 0 ; i<n; i++) {
+                   cin>>vector_b[i];
+               }
+            return product;
+            
+        }else if (n>5){
+cout<<"generate the  values of vector_a:"<<endl;
+cout<<"generate the  values of vector_b:"<<endl;
+for (int i = 0 ; i>n; i++){
+
+random = (rand()%(max-min+1) )+ min;
+vector_a[i] = random;
+random = (rand()%(max-min+1) )+ min;
+vector_b[i] = random;
+cout<< vector_a[i]<<endl;
+cout<<vector_b[i]<<endl;
+      
+  }
+  
+    
+ }
+*/
+        
+
+ double   dot_p( double  vector_a[], double  vector_b[], double   n){
+    double   product  = 0;
+    for (int i = 0; i < n; i++){
+        product += vector_a[i] * vector_b[i];
+    }
+    return product;
+}
+int main(){
+  
+    int max = 9;
+    int min =1;
+    srand((unsigned) time(0));
+   // int  n = (rand()%(rand()+1) )+ 1;
+     int  n = (rand()%(max-min+1) )+ min;
+    cout<<"the value of n :"<< n<<endl;
+    double vector_a[n];
+    double vector_b[n];
+    double random;
+        if(n<=5){
+            cout<<"enter the values of vector_a:"<<endl;
+               for (int i = 0 ; i<n; i++) {
+                   cin>>vector_a[i];
+               }
+               cout<<"enter the values of vector_b"<<endl;
+               for (int i = 0 ; i<n; i++) {
+                   cin>>vector_b[i];
+               }
+        }else if (n>5){
+  cout<<"generate the  values of vector_a:"<<endl;
+  
+  for (int i = 0 ; i<n; i++){
+      
+      random = (rand()%(max-min+1) )+ min;
+      vector_a[i] = random;
+      
+     cout<< vector_a[i]<<" ";
+  }
+            cout<<"generate the  values of vector_b:"<<endl;
+            for (int i = 0 ; i<n; i++){
+            random = (rand()%(max-min+1) )+ min;
+            vector_b[i] = random;
+            cout<<vector_b[i]<<"  ";
+  }
+    cout<< "Dot product: "<<(dot_p(   vector_a,   vector_b,n))<<endl;
+    //cout<<"Dot Product:"<<dot_p(n,vector_a,vector_b)endl;
+    return 0;
+        }
+}
