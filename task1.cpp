756d689da589b0cbc0fc3f5e129d443fd424fa15
plaxidia <plaxidiahove@gmail.com>
2021-11-20 23:37:23 +0300

    push

--- task1.cpp
+++ task1.cpp
@@ -3,24 +3,12 @@
 #include <ostream>
 using namespace std;
 
-double   dot_p( double  vector_a[], double  vector_b[], double   n)
-{
-    double   product  = 0;
-    for (int i = 0; i < n; i++)
-    {
-        product += vector_a[i] * vector_b[i];
-    }
-    return product;
-}
-
 void gen_v( double  vector_a[], double  vector_b[],double random)
 { 
-    int range_min;
-    int range_max;
+    int min;
+    int max;
     srand((unsigned) time(0));
-    int n = (rand()%(range_max-range_min+1))+ range_min;
-for (int i = 0 ; i<n; i++) 
-{
+    int n = (rand()%(max-min+1))+ min;
     if(n<=5)
        {
         cout<<"enter the values of vector_a:"<<endl;
@@ -34,26 +22,33 @@ for (int i = 0 ; i<n; i++)
           cin>>vector_b[i];
         }
       }
-
     else if (n>5)
        {
         cout<<"generate the  values of vector_a:"<<endl;
         for (int i = 0 ; i<n; i++)
             {
-              random = (rand()%(range_max-range_min+1) )+ range_min;
+              random = (rand()%(max-min+1) )+ min;
               vector_a[i] = random;
               cout<< vector_a[i]<<" ";
             }
         cout<<"generate the  values of vector_b:"<<endl;
         for (int i = 0 ; i<n; i++)
             {
-              random = (rand()%(range_max-range_min+1) )+ range_min;
+              random = (rand()%(max-min+1) )+ min;
               vector_b[i] = random;
               cout<<vector_b[i]<<"  ";
             }
        }
-   }
  cout<<"the value of n :"<< n<<endl;
+}
+double   dot_p( double  vector_a[], double  vector_b[], double   n)
+{
+    double   product  = 0;
+    for (int i = 0; i < n; i++)
+    {
+        product += vector_a[i] * vector_b[i];
+    }
+    return product;
 }
 int main()
 {
@@ -64,7 +59,10 @@ int main()
     //cout<<"the value of n :"<< n<<endl;
     double vector_a[n];
     double vector_b[n];
-    cout<< "Dot product: "<<dot_p( vector_a,vector_b,n);
+    double random;
+    cout<<" Vector generator :"<<endl;
+    gen_v(vector_a,vector_b,random);
+    cout<< "Dot product: "<<dot_p( vector_a,vector_b,n)<<endl;
     return 0;
         
 }
